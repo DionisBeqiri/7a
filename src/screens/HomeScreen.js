1fdf2ab5d468ba2bb953bdd907d7aadc2b756237
Dionis <dionisbeqiri8@gmail.com>
2025-09-26 19:33:22 +0200

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>HomeScreen</Text>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("About")}
+        style={styles.button}
+      >
+        <Text>Go to About Us</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Shop")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Go to Shop</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    marginBottom: 20
+  },
+   button: {
+    backgroundColor: "blue",
+    padding: 10,
+    borderRadius: 5,
+    width: 150,
+    height: 50,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18
+  }
+});
+
+export default HomeScreen;
